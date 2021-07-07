b818a1f3455df0d45d717ea2f5ae339683bf03b4
Alejandro Gómora <86501233+agomora08@users.noreply.github.com>
2021-07-07 16:29:59 -0500

    GitHub a RStudio Cloud

---
+++ Ejemplo_PULL.R
@@ -0,0 +1,8 @@
+# Este 'script' es de carácter demostrativo solamente; con él se busca
+# ejemplificar la capacidad que tiene GitHub de cargar archivos a RStudio
+# Cloud (función "PULL"). Nótese que para ello se requiere un repositorio
+# que contenga el archivo a compartir y acceso a RStudio Cloud.
+
+print("¡Demostración de cómo se comparte un archivo desde un repositorio de GitHub!")
+
+example.matrix <- matrix(1:16, byrow = 4, ncol = 4)
