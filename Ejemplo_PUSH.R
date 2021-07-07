af02849629ec3159938735ebf35cd4e45674154b
agomora08 <agom.inoffiziell@gmail.com>
2021-07-07 21:27:37 +0000

    RStudio Cloud a GitHub

---
+++ Ejemplo_PUSH.R
@@ -0,0 +1,7 @@
+# El siguiente 'script' es de carácter demostrativo solamente; con él se busca
+# ejemplificar la capacidad que tiene RStudio Cloud para subir archivos R a un
+# repositorio de GitHub existente (función "PUSH").
+
+print("¡Demostración de cómo subir archivos funcionales a GitHub desde RStudio Cloud!")
+
+(example.df <- data.frame(x = letters, y = seq(1, length(letters))))
