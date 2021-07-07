5d1ca738a61147810db27cbbacb53935b248d4fb
agomora08 <agom.inoffiziell@gmail.com>
2021-07-07 21:44:13 +0000

    GitHub a RStudio Cloud - RStudio Cloud a GitHub

--- Ejemplo_PULL.R
+++ Ejemplo_PULL.R
@@ -6,3 +6,12 @@
 print("¡Demostración de cómo se comparte un archivo desde un repositorio de GitHub!")
 
 example.matrix <- matrix(1:16, byrow = 4, ncol = 4)
+
+# Las líneas de código que se muestran a continuación son una actualización del archivo
+# que se cargó desde GitHub. No obstante, la versión que se visualiza en el repositorio
+# es la versión actualizada, pues se aplicó la función "PUSH" para cargar el archivo de
+# vuelta a GitHub.
+
+(example.matrix <- example.matrix + matrix(17:32, byrow = 4, ncol = 4))
+
+print("¡Se realizó la actualización del archivo exitosamente!")
